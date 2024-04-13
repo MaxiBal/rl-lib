f3f884487e9a95fa2d9fb117a8a2c5bf4a01c769
MaxiBal <maxibal@baltel.com>
2024-04-13 18:58:52 -0400

    add neural net functionality + xor test

--- include/net.h
+++ include/net.h
@@ -1,15 +1,23 @@
 #ifndef NET_H
 #define NET_H
 
+#include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 float gradient(float* y);
 
-// Activation function
+// Activation functions
 
 float sigmoid(float);
 float reLU(float);
+
+// Activation function derivatives
+float d_sigmoid(float);
+float d_reLU(float);
+
 
 // Error functions
 
@@ -43,16 +51,19 @@ struct network_t
     struct layer_t* layers;
 };
 
+static void initialize_neuron_weights(struct neuron_t* neuron, size_t out_weights, bool is_input);
+
+
 /// @brief Initializes a new neuron
 /// @param neuron neuron to initialize
 /// @param out_weights number of neurons in the next layer
-void initialize_neuron  (struct neuron_t* neuron, size_t out_weights);
+void initialize_neuron  (struct neuron_t* neuron, size_t out_weights, bool is_input);
 
 /// @brief Initializes a new layer in a network
 /// @param layer layer to initialize
 /// @param size number of neurons in the layer
 /// @param next_layer_size number of neurons in the next layer
-void initialize_layer   (struct layer_t* layer, size_t size, size_t next_layer_size);
+void initialize_layer   (struct layer_t* layer, size_t size, size_t next_layer_size, bool is_input);
 
 /// @brief Initializes a new neural network
 /// @param network network to initialize
@@ -60,11 +71,26 @@ void initialize_layer   (struct layer_t* layer, size_t size, size_t next_layer_s
 /// @param num_of_layers number of layers
 void initialize_network (struct network_t* network, size_t* layer_sizes, size_t num_of_layers);
 
-
-
 void free_neuron    (struct neuron_t* neuron);
 void free_layer     (struct layer_t* layer);
 void free_network   (struct network_t* network);
 
+
+void feed_input(struct layer_t* input_layer, float** data, size_t n);
+
+float compute_cost(struct layer_t* output_layer, float** labels, size_t n, size_t n_data);
+
+void forward_propogate(struct network_t* network);
+
+static void backpropogate_output_layer(struct layer_t* layers, const size_t output_layer_n, float** labels, size_t label_n);
+static void backpropogate_hidden_layers(struct layer_t* layers, size_t end_layer);
+
+/// @brief Back propogates labels to entire network
+/// @param network network to perform back propogation on
+/// @param labels output data
+/// @param label_n size of ```labels```
+void back_propogate(struct network_t* network, float** labels, size_t label_n);
+
+void update_weights(struct network_t* network, float learning_rate);
 
 #endif
--- src/net.c
+++ src/net.c
@@ -1,15 +1,54 @@
 #include "net.h"
 
+// Activation Functions
 
+float sigmoid(float x)
+{
+    return 1 / (1 + expf(-x));
+}
 
-void initialize_neuron  (struct neuron_t* neuron, size_t out_weights)
+float d_sigmoid(float x)
+{
+    return sigmoid(x) * (1 - sigmoid(x));
+}
+
+float reLU(float x)
+{
+    if (x <= 0) return 0;
+    return x;
+}
+
+float d_reLU(float x)
+{
+    return (float) x > 0;
+}
+
+// Neural Network Initializations, Destructors
+
+static void initialize_neuron_weights(struct neuron_t* neuron, size_t out_weights, bool is_input)
+{
+    for (size_t i = 0; i < out_weights; i++)
+    {
+        neuron->out_weights[i] = ((float) rand()) / ((float) RAND_MAX);
+        neuron->dw[i] = 0.0;
+    }
+
+    if (!is_input)
+    {
+        neuron->bias = ((float) rand()) / ((float) RAND_MAX);
+    }
+}
+
+void initialize_neuron  (struct neuron_t* neuron, size_t out_weights, bool is_input)
 {
     neuron->out_weights = (float*) malloc(out_weights * sizeof(float));
     neuron->dw          = (float*) malloc(out_weights * sizeof(float));
     neuron->out_size    = out_weights;
+
+    initialize_neuron_weights(neuron, out_weights, is_input);
 }
 
-void initialize_layer   (struct layer_t* layer, size_t size, size_t next_layer_size)
+void initialize_layer   (struct layer_t* layer, size_t size, size_t next_layer_size, bool is_input)
 {
     layer->n = size;
     layer->next_layer_size = next_layer_size;
@@ -19,7 +58,7 @@ void initialize_layer   (struct layer_t* layer, size_t size, size_t next_layer_s
     // populate neurons
     for (size_t i = 0; i < size; i++)
     {
-        initialize_neuron(&layer->neurons[i], next_layer_size);
+        initialize_neuron(&layer->neurons[i], next_layer_size, is_input);
     }
 }
 
@@ -28,16 +67,16 @@ void initialize_network (struct network_t* network, size_t* layer_sizes, size_t 
     network->n = num_of_layers;
     network->layers = (struct layer_t*) malloc(num_of_layers * sizeof(struct layer_t));
 
-    for (size_t i = 0; i < num_of_layers - 1; i++)
+    initialize_layer(&network->layers[0], layer_sizes[0], layer_sizes[1], true);
+
+    for (size_t i = 1; i < num_of_layers - 1; i++)
     {
-        initialize_layer(&network->layers[i], layer_sizes[i], layer_sizes[i + 1]);
+        initialize_layer(&network->layers[i], layer_sizes[i], layer_sizes[i + 1], false);
     }
 
     // initialize last layer with 0 output weights
-    initialize_layer(&network->layers[num_of_layers - 1], layer_sizes[num_of_layers - 1], 0);
+    initialize_layer(&network->layers[num_of_layers - 1], layer_sizes[num_of_layers - 1], 0, false);
 }
-
-
 
 void free_neuron    (struct neuron_t* neuron)
 {
@@ -59,4 +98,143 @@ void free_network   (struct network_t* network)
     {
         free_layer(&network->layers[i]);
     }
+}
+
+// Neural network functions
+
+void feed_input(struct layer_t* input_layer, float** data, size_t n)
+{
+    for (size_t i = 0; i < input_layer->n; i++)
+    {
+        input_layer->neurons[i].actv = data[n][i];
+    }
+}
+
+void forward_propogate(struct network_t* network)
+{
+    // Iterate through each layer
+
+    // accessor for layers
+    struct layer_t* layers = network->layers;
+
+    for (size_t l = 1; l < network->n; l++)
+    {
+        // for each neuron in layer l
+        for (size_t n = 0; n < layers[l].n; n++)
+        {
+            layers[l].neurons[n].z = layers[l].neurons[n].bias;
+
+            // calculate neuron z based on last layer's output from each neuron
+            for (size_t i = 0; i < layers[l - 1].n; i++)
+            {
+                layers[l].neurons[n].z += 
+                    layers[l - 1].neurons[i].actv * 
+                    layers[l - 1].neurons[i].out_weights[n]; // out weight for neuron n
+            }
+
+            // use ReLU activation function for each hidden layer
+            if (l < network->n - 1)
+            {
+                layers[l].neurons[n].actv = reLU(layers[l].neurons[n].z);
+            }
+
+            else // use sigmoid
+            {
+                layers[l].neurons[n].actv = sigmoid(layers[l].neurons[n].z);
+            }
+        }
+    }
+}
+
+void back_propogate(struct network_t* network, float** labels, size_t label_n)
+{
+    // accessor for layers
+    struct layer_t* layers = network->layers;
+
+    // start from output layer
+    backpropogate_output_layer(layers, network->n - 1, labels, label_n);
+
+    // backpropogate to all hidden layers
+    backpropogate_hidden_layers(layers, network->n - 2);
+    
+
+}
+
+static void backpropogate_output_layer(struct layer_t* layers, const size_t output_layer_n, float** labels, size_t label_n)
+{
+
+    
+    for (size_t n = 0; n < layers[output_layer_n].n; n++)
+    {
+        
+        layers[output_layer_n].neurons[n].dz = 
+            (layers[output_layer_n].neurons[n].actv - labels[label_n][n]) * d_sigmoid(layers[output_layer_n].neurons[n].actv);
+
+        for (size_t k = 0; k < layers[output_layer_n - 1].n; k++)
+        {
+            layers[output_layer_n - 1].neurons[k].dw[n] = 
+                layers[output_layer_n - 1].neurons[k].actv * layers[output_layer_n].neurons[n].dz;
+
+            layers[output_layer_n - 1].neurons[k].dactv = 
+                layers[output_layer_n - 1].neurons[k].out_weights[n] * layers[output_layer_n].neurons[n].dz;
+        }
+
+        layers[output_layer_n].neurons[n].dbias = layers[output_layer_n].neurons[n].dz;
+    }
+}
+
+static void backpropogate_hidden_layers(struct layer_t* layers, size_t end_layer)
+{
+    for (size_t l = end_layer; l > 0; l--)
+    {
+        // for each neuron in layer
+        for (size_t n = 0; n < layers[l].n; n++)
+        {
+            layers[l].neurons[n].dz = (layers[l].neurons[n].z >= 0) ? layers[l].neurons[n].dactv : 0;
+
+            for (size_t k = 0; k < layers[l - 1].n; k++)
+            {
+                layers[l - 1].neurons[k].dw[n] = layers[l].neurons[n].dz * layers[l - 1].neurons[k].actv;
+
+                if (l > 1)
+                {
+                    layers[l - 1].neurons[k].dactv = layers[l - 1].neurons[k].out_weights[n] * layers[l].neurons[n].dz;
+                }
+            }
+
+            layers[l].neurons[n].dbias = layers[l].neurons[n].dz;
+        }
+    }
+}
+
+void update_weights(struct network_t* network, float learning_rate)
+{
+    struct layer_t* layers = network->layers;
+    for (size_t l = 0; l < network->n - 1; l++)
+    {
+        for (size_t n = 0; n < layers[l].n; n++)
+        {
+            for (size_t k = 0; k < layers[l+1].n; k++)
+            {
+                layers[l].neurons[n].out_weights[k] = 
+                    layers[l].neurons[n].out_weights[k] - (learning_rate * layers[l].neurons[n].dw[k]);
+            }
+
+            // update bias
+
+            layers[l].neurons[n].bias = layers[l].neurons[n].bias - (learning_rate * layers[l].neurons[n].dbias);
+        }
+    }
+}
+
+float compute_cost(struct layer_t* output_layer, float** labels, size_t n, size_t n_data)
+{
+    float cost = 0.0;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        cost += powf(labels[n_data][i] - output_layer->neurons[i].actv, 2.0) / 2;
+    }
+
+    return cost;
 }
